f5542b0a4ec13e5fe1c018c9318da77c00949d31
Enes Ergen <eenes44@gmail.com>
2023-05-22 15:31:31 +0300

    firstname,lastname and email was added in Login response

--- src/main/java/com/enesergen/fashionbackend/fashion/auth/AuthenticationResponse.java
+++ src/main/java/com/enesergen/fashionbackend/fashion/auth/AuthenticationResponse.java
@@ -11,4 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationResponse {
     private String token;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
--- src/main/java/com/enesergen/fashionbackend/fashion/auth/AuthenticationService.java
+++ src/main/java/com/enesergen/fashionbackend/fashion/auth/AuthenticationService.java
@@ -41,6 +41,6 @@ public class AuthenticationService {
         ));
         var user=userRepository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken=jwtService.generateToken(user);
-        return AuthenticationResponse.builder().token(jwtToken).build();
+        return AuthenticationResponse.builder().token(jwtToken).firstName(user.getFirstName()).lastName(user.getLastName()).email(user.getEmail()).build();
     }
 }
